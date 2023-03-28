d454f94e3439be07e7bacd6795776309fef96d7a
Thone Liu <thorne_lew@126.com>
2023-03-28 20:50:43 +0800

    modify picom

--- autostart.sh
+++ autostart.sh
@@ -7,7 +7,7 @@ pulseaudio --start
 fcitx5 -d
 /bin/bash ~/.dwm/scripts/dwm-status.sh &
 /bin/bash ~/.dwm/scripts/window-bg-autochange.sh &
-picom -b
+picom --experimental-backend -b
 xfce4-power-manager &
 redshift -l 39.916668:116.383331 &
 xautolock -time 30 -locker slock
